3d6e8cf261b9e2b726e1e9fede35b9503248226f
ws-git <ws-git@caelum11-sala3.(none)>
2011-10-18 19:37:18 -0200

    estrutura inicial do projeto

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CadastroDePessoas::Application.config.secret_token = 'd8714e0803e78c086ab67f046c46a189973f281cba14267d52a0069cfa8a9f1b742de8543e6b863bff1680b6e7ad6855a9d8081f96e6e4cb17cbf679db9e42d2'
